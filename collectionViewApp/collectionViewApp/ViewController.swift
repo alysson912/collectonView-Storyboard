75ad3f9b0891817065a86c8a844dc3b0030e2a8e
Alysson Menezes <almenezes912@gmail.com>
2023-02-20 16:57:39 -0300

    projeto em inicial

---
+++ collectionViewApp/collectionViewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  collectionViewApp
+//
+//  Created by ALYSSON DODO on 20/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
